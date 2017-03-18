8a36a26d2ae4ea6b96ebf62a758164d4082fdea7
PhillRob <robeck-p@email.ulster.ac.uk>
2017-03-18 23:59:36 +1100

    ref

--- archive-completed-issue.py
+++ archive-completed-issue.py
@@ -3,14 +3,14 @@ from datetime import datetime, timedelta
 
 from fulcrum import Fulcrum
 
-# variables
+# variables ##thum
 print('set variables')
-formIdIssue = '52d56fc5-0a83-4912-a0aa-cbea3f8fc0ff'
+formIdIssue = '58172326-272a-49d8-916e-7a893fd52dd4'
 # formIdTest = '345086c9-0add-4a4b-a458-62fbfef58743'# get form ID from fulcrum web app
-apiToken = "magic api token"  # magic api token
+apiToken = "2ef04ab67ab414b7ac6c7815235ace6f9cdf3ab79b9e0874beb053a9dfa9bb682d7bbd1a3117b68e"  # magic api token
 urlBase = 'https://api.fulcrumapp.com/api/v2/'
 fulcrum = Fulcrum(key=apiToken)
-logging.basicConfig(filename='status-update.log', level=logging.DEBUG)
+logging.basicConfig(filename='thm-issue-status-update.log', level=logging.DEBUG)
 issueTimestamp = datetime.today() - timedelta(days=15)  # number of days as a delimiter
 
 print('get issue data')
@@ -18,15 +18,12 @@ Issue = fulcrum.records.search(url_params={'form_id': formIdIssue})['records']
 
 print('create issue dataframe for roads')
 for record in Issue:
-    if '9b3a' not in record['form_values']:
-        # Issue[index]['osname'] = 'NA'
-        print('No OS name field found')
+    if record['status'] == "Archived":
+        print("already archived")
     else:
-        if (record['updated_at'] <= str(issueTimestamp)) & (record['status'] == "Completed"):
-            # Issue[index]['osname'] = ''.join(Issue[index]['form_values']['9b3a'])
+        if (record['updated_at'] <= str(issueTimestamp)) & (record['status'] == "No Action Required"):
             record['status'] = "Archived"
             updatedRecord = fulcrum.records.update(record['id'], record)
-            print(record['updated_at'], 'OS name found, older than', issueTimestamp, 'and status completed')
+            print(record['updated_at'], 'older than', issueTimestamp, 'and status no action required')
         else:
             print('Record not completed or younger than', issueTimestamp)
-            record['osname'] = 'NA'
