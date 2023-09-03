4b517edf5b3b4ca44d04c12fe2d46062238b21ad
Philipp <robeck-p@email.ulster.ac.uk>
2023-09-03 11:16:58 +0200

    clean up and comments

--- delete-records.py
+++ delete-records.py
@@ -9,33 +9,24 @@ with open("/credentials.json") as c:
 	credentials = json.load(c)
 
 fulcrum = Fulcrum(key=credentials['fulcrum_api'])
-
-# fuclrum from
-formID = "6d9c5cdf-f947-48b8-9a99-0ec49b492d77"
-formdata = fulcrum.forms.find(formID)
 recordsPerPage = 5000
 
 # get number of pages
-recordCount = fulcrum.forms.find(formID)['form']['record_count']
+recordCount = fulcrum.forms.find(credentials['TMO_TI_form'])['form']['record_count']
 pages = math.ceil(recordCount / recordsPerPage)
 
 # get data
-if pages > 1:
-	for p in range(1, pages + 1):
-		dataPage = fulcrum.records.search(url_params={'form_id': formID, 'page': p, 'per_page': recordsPerPage})[
-			'records']
-		if p > 1:
-			data.extend(dataPage)
-		else:
-			data = dataPage
-else:
-	data = fulcrum.records.search(url_params={'form_id': formID, 'page': 1, 'per_page': recordsPerPage})['records']
+data = []
+for p in range(1, pages + 1):
+	dataPage = fulcrum.records.search(
+		url_params={'form_id': credentials['TMO_TI_form'], 'page': p, 'per_page': recordsPerPage})['records']
+	data.extend(dataPage)
 
-noloc = missmatch = match = 0
+missmatch = match = 0
 
+# this deletes everything, add a little bit of logic
 for record in data:
 	deleted = fulcrum.records.delete(record['id'])
-	match = match+1
+	match = match + 1
 	print(match)
-
 
